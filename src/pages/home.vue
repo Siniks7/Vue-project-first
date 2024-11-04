601848e6cedc9191cd01ab04997c1de3905a30dd
Nikolay <siniks7@yandex.ru>
2024-11-04 22:05:26 +0700

    Лист заметок

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,26 @@
 <template>
   <Form @onSubmit="handleSubmit" />
+  <List @onRemove="deleteNote" :items="notes" />
 </template>
 
 <script>
 import Form from '@/components/Notes/Form.vue'
+import List from '@/components/Notes/List.vue'
 export default {
   name: 'App',
-  components: { Form },
+  components: { Form, List },
+  data() {
+    return {
+      notes: ['task1', 'task2', 'task3']
+    }
+  },
   methods: {
     handleSubmit(note) {
+      this.notes.push(note)
       console.log(note)
+    },
+    deleteNote(index) {
+      this.notes.splice(index, 1)
     }
   }
 }
