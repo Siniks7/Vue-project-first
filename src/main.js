581646d361680f23d7360d2d32951a74ed1df65a
Nikolay <siniks7@yandex.ru>
2024-11-10 11:39:02 +0700

    Создания хранилища vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue';
-import '../src/assets/scss/main.scss';
-import App from './App.vue';
-import routers from './routes';
-const app = createApp(App);
+import { store } from '@/store/index'
+import { createApp } from 'vue'
+import '../src/assets/scss/main.scss'
+
+import App from './App.vue'
+import routers from './routes'
+const app = createApp(App)
 app.use(routers)
+app.use(store)
 app.mount('#app')
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,56 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <Form @onSubmit="handleSubmit" />
-  <List @onRemove="deleteNote" :items="notes" />
+  <p>vuex</p>
 </template>
 
 <script>
-import Form from '@/components/Notes/Form.vue'
-import List from '@/components/Notes/List.vue'
-export default {
-  name: 'App',
-  components: { Form, List },
-  data() {
-    return {
-      notes: [
-        {
-          title: 'Learn Vue 3',
-          tags: ['work']
-        },
-        {
-          title: 'Finish course',
-          tags: ['work', 'home']
-        }
-      ]
-    }
-  },
-  mounted() {
-    this.getNotes()
-  },
-  watch: {
-    notes: {
-      handler(updatedList) {
-        localStorage.setItem('notes', JSON.stringify(updatedList))
-      },
-      deep: true
-    }
-  },
-  methods: {
-    getNotes() {
-      const localNotes = localStorage.getItem('notes')
-      if (localNotes) {
-        this.notes = JSON.parse(localNotes)
-      }
-    },
-    handleSubmit(note) {
-      const newObject = {
-        title: note,
-        tags: []
-      }
-      this.notes.push(newObject)
-    },
-    deleteNote(index) {
-      this.notes.splice(index, 1)
-    }
-  }
-}
+export default {}
 </script>
